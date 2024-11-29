93952134fec01018ed341edfbf1eeeee28584051
hjc2000 <hjc.666@foxmail.com>
2024-11-29 10:19:58 +0800

    lfs_config const &Port() const;

--- include/littlefs/LfsFlashPort.cpp
+++ include/littlefs/LfsFlashPort.cpp
@@ -3,4 +3,46 @@
 Lfs::LfsFlashPort::LfsFlashPort(std::string const &flash_name)
 {
     _flash = DI_FlashCollection().Get(flash_name);
+
+    _lfs.context = this;
+    _lfs.read_size = 1;
+    _lfs.prog_size = _flash->ProgrammingSize();
+    _lfs.block_size = _flash->SectorSize();
+
+    _lfs.read = [](lfs_config const *c, lfs_block_t block,
+                   lfs_off_t off, void *buffer, lfs_size_t size) -> int
+    {
+        return 0;
+    };
+
+    _lfs.prog = [](lfs_config const *c, lfs_block_t block,
+                   lfs_off_t off, void const *buffer, lfs_size_t size) -> int
+    {
+        return 0;
+    };
+
+    _lfs.erase = [](lfs_config const *c, lfs_block_t block) -> int
+    {
+        return 0;
+    };
+
+    _lfs.sync = [](lfs_config const *c) -> int
+    {
+        return 0;
+    };
+
+    _lfs.lock = [](lfs_config const *c) -> int
+    {
+        return 0;
+    };
+
+    _lfs.unlock = [](lfs_config const *c) -> int
+    {
+        return 0;
+    };
+}
+
+lfs_config const &Lfs::LfsFlashPort::Port() const
+{
+    return _lfs;
 }
--- include/littlefs/LfsFlashPort.h
+++ include/littlefs/LfsFlashPort.h
@@ -9,10 +9,15 @@ namespace Lfs
     {
     private:
         bsp::IFlash *_flash = nullptr;
+        lfs_config _lfs{};
 
     public:
         /// @brief
         /// @param flash_name 要用来实现 lfs_config 接口的 flash 的名称。
         LfsFlashPort(std::string const &flash_name);
+
+        /// @brief 获取被本类包装的 littlefs 库所需要的端口 —— lfs_config.
+        /// @return
+        lfs_config const &Port() const;
     };
 } // namespace Lfs

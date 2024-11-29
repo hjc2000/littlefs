9745b042d37757e61095412c4441ec3595c41cd0
hjc2000 <hjc.666@foxmail.com>
2024-11-29 11:28:39 +0800

    LfsFlashPort

--- include/littlefs/LfsFlashPort.cpp
+++ include/littlefs/LfsFlashPort.cpp
@@ -7,33 +7,87 @@ void Lfs::LfsFlashPort::InitializeLfsPort()
     _lfs.read = [](lfs_config const *c, lfs_block_t block,
                    lfs_off_t off, void *buffer, lfs_size_t size) -> int
     {
-        return 0;
+        try
+        {
+            LfsFlashPort *self = reinterpret_cast<LfsFlashPort *>(c->context);
+            self->_flash->Read(block, off, reinterpret_cast<uint8_t *>(buffer), size);
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.prog = [](lfs_config const *c, lfs_block_t block,
                    lfs_off_t off, void const *buffer, lfs_size_t size) -> int
     {
-        return 0;
+        try
+        {
+            LfsFlashPort *self = reinterpret_cast<LfsFlashPort *>(c->context);
+            lfs_size_t have_write = 0;
+            while (have_write < size)
+            {
+                self->_flash->Program(block, off, reinterpret_cast<uint8_t const *>(buffer));
+                have_write += self->_flash->ProgrammingSize();
+            }
+
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.erase = [](lfs_config const *c, lfs_block_t block) -> int
     {
-        return 0;
+        try
+        {
+            LfsFlashPort *self = reinterpret_cast<LfsFlashPort *>(c->context);
+            self->_flash->EraseSector(block);
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.sync = [](lfs_config const *c) -> int
     {
-        return 0;
+        try
+        {
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.lock = [](lfs_config const *c) -> int
     {
-        return 0;
+        try
+        {
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.unlock = [](lfs_config const *c) -> int
     {
-        return 0;
+        try
+        {
+            return lfs_error::LFS_ERR_OK;
+        }
+        catch (...)
+        {
+            return lfs_error::LFS_ERR_IO;
+        }
     };
 
     _lfs.read_size = 1;
@@ -41,8 +95,8 @@ void Lfs::LfsFlashPort::InitializeLfsPort()
     _lfs.block_size = _flash->SectorSize();
     _lfs.block_count = _flash->SectorCount();
     _lfs.block_cycles = 100;
-    _lfs.cache_size = 32;
-    _lfs.lookahead_size = _flash->SectorCount() / 8;
+    _lfs.cache_size = 128;
+    _lfs.lookahead_size = 128;
 }
 
 Lfs::LfsFlashPort::LfsFlashPort(std::string const &flash_name)

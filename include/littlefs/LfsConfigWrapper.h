78ab0bf550863eb7ba89ced0bba490eaf5b9f42a
hjc2000 <hjc.666@foxmail.com>
2024-11-29 10:06:33 +0800

    LfsConfigWrapper

---
+++ include/littlefs/LfsConfigWrapper.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <bsp-interface/di/flash.h>
+#include <lfs.h>
+
+namespace Lfs
+{
+    /// @brief 对 lfs_config 的包装
+    class LfsConfigWrapper
+    {
+    public:
+    };
+} // namespace Lfs

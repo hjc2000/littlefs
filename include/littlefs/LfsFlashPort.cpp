4fc7cebea8666075758aa13552d079b1dfadf6ad
hjc2000 <hjc.666@foxmail.com>
2024-12-02 09:25:42 +0800

    TestLittleFs

--- include/littlefs/LfsFlashPort.cpp
+++ include/littlefs/LfsFlashPort.cpp
@@ -102,13 +102,86 @@ void Lfs::LfsFlashPort::InitializeLfsPort()
     _lfs.lookahead_size = 128;
 }
 
-Lfs::LfsFlashPort::LfsFlashPort(std::string const &flash_name)
+Lfs::LfsFlashPort::LfsFlashPort(bsp::IFlash *flash)
 {
-    _flash = DI_FlashCollection().Get(flash_name);
+    _flash = flash;
     InitializeLfsPort();
 }
 
 lfs_config const &Lfs::LfsFlashPort::Port() const
 {
     return _lfs;
+}
+
+#include <bsp-interface/flash/RmaFlash.h>
+
+void Lfs::TestLittleFs()
+{
+    // Lfs::LfsFlashPort port{DI_FlashCollection().Get("internal-flash")};
+    // Lfs::LfsFlashPort port{DI_FlashCollection().Get("internal-flash")};
+
+    bsp::RmaFlash ram_flash{
+        "flash",
+        base::Span{
+            reinterpret_cast<uint8_t *>(0XC0000000),
+            32 * 1024 * 1024,
+        },
+    };
+
+    Lfs::LfsFlashPort port{&ram_flash};
+
+    // 初始化 LittleFS 文件系统
+    lfs_t lfs;
+    int res = lfs_format(&lfs, &port.Port()); // 格式化文件系统
+    if (res)
+    {
+        DI_Console().WriteLine("format failed: " + std::to_string(res));
+    }
+
+    res = lfs_mount(&lfs, &port.Port()); // 挂载文件系统
+    if (res)
+    {
+        DI_Console().WriteLine("mount failed: " + std::to_string(res));
+    }
+
+    // 创建文件并写入数据
+    char const *filename = "example.txt";
+    char const *str = "Hello, LittleFS!";
+    lfs_file_t file;
+
+    res = lfs_file_open(&lfs, &file, filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
+    if (res)
+    {
+        DI_Console().WriteLine("open failed: " + std::to_string(res));
+    }
+
+    res = lfs_file_write(&lfs, &file, str, strlen(str));
+    if (res < 0)
+    {
+        DI_Console().WriteLine("write failed: " + std::to_string(res));
+    }
+
+    lfs_file_close(&lfs, &file);
+
+    // 读取文件内容
+    res = lfs_file_open(&lfs, &file, filename, LFS_O_RDONLY);
+    if (res)
+    {
+        DI_Console().WriteLine("open failed: " + std::to_string(res));
+    }
+
+    char buffer[128] = {0};
+    res = lfs_file_read(&lfs, &file, buffer, sizeof(buffer) - 1);
+    if (res < 0)
+    {
+        DI_Console().WriteLine("read failed: " + std::to_string(res));
+    }
+
+    DI_Console().WriteLine("have read: " + std::to_string(res));
+    DI_Console().WriteLine(buffer);
+
+    lfs_file_close(&lfs, &file);
+
+    // 卸载文件系统
+    lfs_unmount(&lfs);
 }
--- include/littlefs/LfsFlashPort.h
+++ include/littlefs/LfsFlashPort.h
@@ -15,72 +15,12 @@ namespace Lfs
         void InitializeLfsPort();
 
     public:
-        /// @brief
-        /// @param flash_name 要用来实现 lfs_config 接口的 flash 的名称。
-        LfsFlashPort(std::string const &flash_name);
+        LfsFlashPort(bsp::IFlash *flash);
 
         /// @brief 获取被本类包装的 littlefs 库所需要的端口 —— lfs_config.
         /// @return
         lfs_config const &Port() const;
     };
 
-    inline void TestLittleFs()
-    {
-        Lfs::LfsFlashPort port{"internal-flash"};
-
-        // 初始化 LittleFS 文件系统
-        lfs_t lfs;
-        int res = lfs_format(&lfs, &port.Port()); // 格式化文件系统
-        if (res)
-        {
-            DI_Console().WriteLine("format failed: " + std::to_string(res));
-        }
-
-        res = lfs_mount(&lfs, &port.Port()); // 挂载文件系统
-        if (res)
-        {
-            DI_Console().WriteLine("mount failed: " + std::to_string(res));
-        }
-
-        // 创建文件并写入数据
-        char const *filename = "example.txt";
-        char const *str = "Hello, LittleFS!";
-        lfs_file_t file;
-
-        res = lfs_file_open(&lfs, &file, filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
-        if (res)
-        {
-            DI_Console().WriteLine("open failed: " + std::to_string(res));
-        }
-
-        res = lfs_file_write(&lfs, &file, str, strlen(str));
-        if (res < 0)
-        {
-            DI_Console().WriteLine("write failed: " + std::to_string(res));
-        }
-
-        lfs_file_close(&lfs, &file);
-
-        // 读取文件内容
-        res = lfs_file_open(&lfs, &file, filename, LFS_O_RDONLY);
-        if (res)
-        {
-            DI_Console().WriteLine("open failed: " + std::to_string(res));
-        }
-
-        char buffer[128] = {0};
-        res = lfs_file_read(&lfs, &file, buffer, sizeof(buffer) - 1);
-        if (res < 0)
-        {
-            DI_Console().WriteLine("read failed: " + std::to_string(res));
-        }
-
-        DI_Console().WriteLine("have read: " + std::to_string(res));
-        DI_Console().WriteLine(buffer);
-
-        lfs_file_close(&lfs, &file);
-
-        // 卸载文件系统
-        lfs_unmount(&lfs);
-    }
+    void TestLittleFs();
 } // namespace Lfs
